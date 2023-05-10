aedd3c02a6990e45dff0a6ea89b60c0841f81485
knyfic <109595809+knyfic@users.noreply.github.com>
2023-05-10 23:37:53 +0300

    Update install.js

--- parabollat/install.js
+++ parabollat/install.js
@@ -4,15 +4,16 @@ pip install pygobject matplotlib numpy
 
 echo "[Desktop Entry]
 Name=Parabollat
-Exec=python3 $HOME/.local/share/parabollat/parabollat.py
-Icon=$HOME/.local/share/parabollat/parabollat.png
+Exec=python3 $HOME/.local/share/parabollat/parabollatparabollat.py
+Icon=$HOME/.local/share/parabollat/parabollat/parabollat.png
 Terminal=false
 Type=Application
 Categories=GTK;Application;" > parabollat.desktop
 
  cp parabollat.desktop $HOME/.local/share/applications/
  
- chmod +755 $HOME/.local/share/parabollat.py
+ chmod +755 $HOME/.local/share/parabollat/parabollat/parabollat.py
+ chmod +755 $HOME/.local/share/applications/parabollat.desktop
 
 echo "installation complete.please do not change the directory where you installed the app. otherwise the application will not work.desktop has been created, you can open the parabollota from your application menu.
 "
